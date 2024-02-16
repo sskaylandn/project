9403687bfd0487dd93b558e62cc1a20828ef351b
sskaylandn <87233715+sskaylandn@users.noreply.github.com>
2024-02-16 09:53:50 +0700

    Add files via upload

--- resources/views/user/akuisisi/index.blade.php
+++ resources/views/user/akuisisi/index.blade.php
@@ -10,32 +10,29 @@
         <div class="table-responsive text-nowrap">
             <table class="table">
               <thead>
-                <tr>
+                <tr> 
                   <th>No</th>
                   <th>Tanggal Efektif</th>
                   <th>Nomor Dokumen</th>
                   <th>Kategori Dokumen</th>
                   <th>Versi</th>
                   <th>Detail Monitoring</th>
+                  <th>Aksi</th>
                 </tr>
               </thead>
               <tbody class="table-border-bottom-0">
-                <tr>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
-                  <td>
-                  </td>
+                @php $no = 1; @endphp
+                  @foreach ($data_akuisisi as $item)
+                  <tr>
+                    <td>{{$no++}}</td>
+                    <td>{{$item->tgl_efektif}}</td>
+                    <td>{{$item->nomor_dokumen}}</td>
+                    <td>{{$item->kategori_dokumen}}</td>
+                    <td>{{$item->versi}}</td>
+                    <td><a href="{{ url('user/detail-akuisisi', $item->id_akuisisi) }}"><span class="btn btn-info rounded-2 fw-semibold">Detail</span></a></td>
+                    <td><a href="{{ url('user/edit-akuisisi', $item->id_akuisisi) }}"><span class="btn btn-primary rounded-2 fw-semibold">Edit</span></a></td>
                 </tr>
+                @endforeach
               </tbody>
             </table>
         </div>
